26eae89704d76472a44abea4374c126833cd7979
Mikhail Tkachev <tritium.mkl@gmail.com>
2023-06-05 13:43:28 +0600

    Удаление console.log-ов

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -48,7 +48,6 @@ class AuthState extends StoreModule {
    * Сброс авторизации пользователя
    */
   async logout() {
-    // console.log('in')
     const response = await fetch(`/api/v1/users/sign`, {
       method: 'DELETE',
       headers: {
@@ -56,7 +55,6 @@ class AuthState extends StoreModule {
         'X-Token': this.getState().token,
       },
     })
-    // console.log(response)
     if (response.ok) {
       localStorage.removeItem('token');
       localStorage.removeItem('userId');
